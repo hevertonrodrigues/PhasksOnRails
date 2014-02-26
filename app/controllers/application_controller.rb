c5a08bee0a2099e78e76939fe3255c9174f31c4a
Heverton Rodrigues <heverton25@gmail.com>
2014-02-26 14:56:35 -0300

    criado menu automatico

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+	before_action :create_menu
+
+
+
+
+
+	def create_menu
+    @data_tables = ActiveRecord::Base.connection.tables
+	end
+
+
+
 end
