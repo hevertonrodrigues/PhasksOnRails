8db4f9bb469d2a9cefb2704416741de20e2f8c86
Thiago S.F <thiagosfsjc@gmail.com>
2014-03-01 16:45:31 -0300

    Adicionando breadcrumb e alterando templates scaffold

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -2,26 +2,35 @@
 module TagsHelper
 	# Link de voltar
 	def back_link(path)
-		link_to '<i class="fa fa-long-arrow-left"></i> '.html_safe + t("site.back"), path, :class => 'btn btn-default'
+		link_to '<i class="glyphicon glyphicon-chevron-left"></i> '.html_safe + t("site.back"), path, :class => 'btn btn-default'
 	end
 
 	# Link para adicionar
 	def add_link(path)
-		link_to '<i class="fa fa-plus"></i>'.html_safe, path, :class => 'btn btn-primary', :title => t('site.new')
+		link_to '<i class="glyphicon glyphicon-plus"></i> '.html_safe + t("site.new"), path, :class => 'btn btn-primary', :title => t('site.new')
 	end
 
 	# Link para visualizar
 	def show_link(path)
-		link_to '<i class="fa fa-search"></i>'.html_safe, path, :class => 'btn btn-info', :title => t('site.show')
+		link_to '<i class="glyphicon glyphicon-search"></i>'.html_safe, path, :class => 'btn btn-info', :title => t('site.show')
 	end
 
 	# Link para editar
 	def edit_link(method)
-		link_to '<i class="fa fa-edit"></i>'.html_safe, method, :class => 'btn btn-success', :title => t('site.edit')
+		link_to '<i class="glyphicon glyphicon-edit"></i>'.html_safe, method, :class => 'btn btn-success', :title => t('site.edit')
 	end
 
 	# Link para deletar
 	def destroy_link(path)
-		link_to '<i class="fa fa-times"></i>'.html_safe, path, method: :delete, data: { confirm: 'Are you sure?' }, :class => 'btn btn-danger', :title => t('site.destroy')
+		link_to '<i class="glyphicon glyphicon-remove"></i>'.html_safe, path, method: :delete, data: { confirm: 'Are you sure?' }, :class => 'btn btn-danger', :title => t('site.destroy')
+	end
+
+	# Breadcrumb
+	def breadcrumb
+		%{<ol class="breadcrumb">
+		  <li>#{link_to t("site.dashboard"), "/"}</li>
+		  <li>#{link_to t("site."+controller.controller_name), "/" + controller.controller_path}</li>
+		  <li>#{t("site."+controller.action_name)}</li>
+		</ol>}.html_safe
 	end
 end
