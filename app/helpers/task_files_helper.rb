ff61a736a27ab5562e93e5859bac14d2acedeb35
Thiago S.F <thiagosfsjc@gmail.com>
2014-03-04 12:48:44 -0300

    Adicionado comportamento para incluir arquivos nao-imagens ao upload nas tarefas

---
+++ app/helpers/task_files_helper.rb
@@ -0,0 +1,10 @@
+module TaskFilesHelper
+  def task_file_file_preview(task_file, size = "50x50")
+    if task_file.content_type_images.include?( task_file.file.content_type ) then
+      image_size = if size == "50x50" then :thumb else :medium end
+      link_to (image_tag task_file.file(image_size), :class => 'img-thumbnail img-responsive'), task_file.file(:original), :class => 'lightbox'
+    else
+      link_to (image_tag task_file.file_icon( task_file.file, size ), :class => 'img-thumbnail img-responsive'), task_file.file(:original), :download => task_file.file_file_name, :target => "_blank"
+    end
+  end
+end
--- app/models/task_file.rb
+++ app/models/task_file.rb
@@ -2,29 +2,43 @@ class TaskFile < ActiveRecord::Base
   default_scope { order('id DESC') } 
   belongs_to :task
   has_attached_file :file, {
-    :styles => {
-      :thumb => ["50x50#", :png],
-      :medium => ["100x100#", :png],
-      :large => ["300x300>", :png]
+    :styles => lambda { |a| content_type_images.include?( a.content_type ) ? { 
+        :thumb => ["50x50#", :png],
+        :medium => ["100x100#", :png],
+        :large => ["300x300>", :png]
+      } : {}  
     },
     :convert_options => {
       :thumb => "-gravity Center -crop 50x50+0+0",
       :medium => "-gravity Center -crop 100x100+0+0"
     },
     :default_url => "http://dummyimage.com/50x50/cccccc/777777.png"
-  } 
-  validates_attachment_content_type :file, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  }
+  validates_attachment_content_type :file, :content_type => /.*/
 
+  # Inclue helpers para model
   include Rails.application.routes.url_helpers
 
+  # Hash para retorno do json do jquery upload
   def to_jq_upload
     {
-      "thumbnail_url" => file.url(:thumb),
+      "thumbnail_url" => if content_type_images.include?( file.content_type ) then file.url(:thumb) else file_icon( file ) end ,
       "name" => read_attribute(:file_file_name),
       "size" => read_attribute(:file_file_size),
       "url" => file.url(:original),
       "delete_url" => task_file_path(self),
       "delete_type" => "DELETE" 
     }
+  end
+
+  # Retorna icon do arquivo
+  def file_icon(file, size = "50x50")
+    extension = File.extname(file.url)[1..-1]
+    "http://dummyimage.com/#{size}/cccccc/777777.png&text=#{extension}"
+  end
+
+  # Retorna content-types de imagens apenas
+  def content_type_images
+    ["image/jpeg", "image/jpg", "image/png", "image/gif"]
   end
 end
