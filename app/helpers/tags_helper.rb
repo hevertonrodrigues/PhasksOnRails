155ee3c0fa6e1859b96e33a0418a6beaf8c42925
Thiago S.F <thiagosfsjc@gmail.com>
2014-03-04 14:41:49 -0300

    Metodo para retornar icone e adaptacoes

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,33 +1,34 @@
 # Adicionando metodos em tempo real na Classe TagsHelper
 module TagsHelper
+	
 	# Link de voltar
 	def back_link(path)
-		link_to '<i class="glyphicon glyphicon-chevron-left"></i> '.html_safe + t("site.back"), path, :class => 'btn btn-default'
+		link_to get_icon('chevron-left') + t("site.back"), path, :class => 'btn btn-default'
 	end
 
 	# Link para adicionar
 	def add_link(path)
-		link_to '<i class="glyphicon glyphicon-plus"></i> '.html_safe + t("site.new"), path, :class => 'btn btn-primary', :title => t('site.new')
+		link_to get_icon('plus') + t("site.new"), path, :class => 'btn btn-primary', :title => t('site.new')
 	end
 
 	# Link para visualizar
 	def show_link(path)
-		link_to '<i class="glyphicon glyphicon-search"></i>'.html_safe, path, :class => 'btn btn-info', :title => t('site.show')
+		link_to get_icon('search'), path, :class => 'btn btn-info', :title => t('site.show')
 	end
 
 	# Link para editar
 	def edit_link(method)
-		link_to '<i class="glyphicon glyphicon-edit"></i>'.html_safe, method, :class => 'btn btn-success', :title => t('site.edit')
+		link_to get_icon('edit'), method, :class => 'btn btn-success', :title => t('site.edit')
 	end
 
 	# Link para deletar
 	def destroy_link(path)
-		link_to '<i class="glyphicon glyphicon-remove"></i>'.html_safe, path, method: :delete, data: { confirm: 'Are you sure?' }, :class => 'btn btn-danger', :title => t('site.destroy')
+		link_to get_icon('remove'), path, method: :delete, data: { confirm: 'Are you sure?' }, :class => 'btn btn-danger', :title => t('site.destroy')
 	end
 
 	# Link para deletar e por ajax e retornar json
 	def destroy_link_remote(path)
-		link_to '<i class="glyphicon glyphicon-remove"></i>'.html_safe, path, "data-type" => :json, :remote => true, method: :delete, data: { confirm: 'Are you sure?' }, :class => 'btn btn-danger', :title => t('site.destroy')
+		link_to get_icon('remove'), path, "data-type" => :json, :remote => true, method: :delete, data: { confirm: 'Are you sure?' }, :class => 'btn btn-danger', :title => t('site.destroy')
 	end
 
 	# Breadcrumb
@@ -37,5 +38,10 @@ module TagsHelper
 		  <li>#{link_to t("site."+controller.controller_name), "/" + controller.controller_path}</li>
 		  <li>#{t("site."+controller.action_name)}</li>
 		</ol>}.html_safe
+	end
+
+	# Retorna icone
+	def get_icon(name)
+		" <i class=\"glyphicon glyphicon-#{name}\" ></i> ".html_safe
 	end
 end
